b6e701a75eb9425d74d554c86930e5265cadbe1b
Alex Cocan <al_razvan@yahoo.com>
2020-11-18 16:57:16 +0100

    updated for deployment

--- server.js
+++ server.js
@@ -27,6 +27,14 @@ mongoose.connect(uri, {
 //     // })
 // }
 
+if (process.env.NODE_ENV === "production"{
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+
+
 const connection = mongoose.connection;
 connection.once('open', ()=>
 console.log("MongoDb connection established successfully"))
